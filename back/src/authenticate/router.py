d23ffabe49c351af980f889233d21cf324a1b095
Renato Guedes <renatoguedesead@gmail.com>
2024-10-20 02:37:01 -0300

    reset router

--- back/src/authenticate/router.py
+++ back/src/authenticate/router.py
@@ -1,24 +1,18 @@
-from fastapi import FastAPI, APIRouter, Request, Response, HTTPException
-from fastapi import status  # Importar status corretamente
+from fastapi import APIRouter, status, Request, Response, HTTPException
 
 from config.request.base_session import requests_session
 from config.logger.base_logger import logger
 from src.authenticate.schemas import ApicLoginSchema
 from src.authenticate.models import AutenticateResponseModel, OkResponse, ErrorResponse
 
-# Definir a aplicação FastAPI
-app = FastAPI()
 
-# Definir rota de status
-@app.get("/status")
-async def get_status():
-    return {"status": "ok"}
 
 autenticate_router = APIRouter(
     prefix="/autenticate",
     tags=["autenticate"],
     responses={404: {"description": "Not found"}},
 )
+
 
 @autenticate_router.post(
     "",
@@ -47,13 +41,13 @@ async def autenticate(
         url = f"https://{ip_address}/api/aaaLogin.json"
         login_dict = {"aaaUser": {"attributes": {"name": login.name, "pwd": login.pwd}}}
         r = requests_session.post(url=url, json=login_dict, verify=False)
-        logger.error(f"request to apic was completed with response {r.json()}")
+        logger.error(f"request to apic was completed with repsonse {r.json()}")
 
         if r.status_code != 200:
             logger.error("status code is not 200")
-            raise HTTPException(
+            raise ErrorResponse(
                 status_code=status.HTTP_403_FORBIDDEN,
-                detail="could not get token from APIC",
+                detail=f"could not get token from APIC",
             )
         response_dict = r.json()
         logger.info(response_dict)
@@ -64,12 +58,8 @@ async def autenticate(
 
         return OkResponse(token=token, status_code=200)
 
-    except Exception as e:
-        logger.error(f"Error occurred: {e}")
-        raise HTTPException(
+    except:
+        raise ErrorResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Request cannot be completed with the current parameters",
+            detail=f"Request cannot be completed with the current parameters",
         )
-
-# Incluir o APIRouter na aplicação FastAPI
-app.include_router(autenticate_router)
