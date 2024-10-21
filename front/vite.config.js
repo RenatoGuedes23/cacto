617bd966050265a093b1885aaf9d7000b583a60c
Renato Guedes <renatoguedesead@gmail.com>
2024-10-21 01:47:27 -0300

    ajustando vite

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,7 +19,10 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['@opentelemetry/sdk-trace-web']
+      external: [
+        '@opentelemetry/sdk-trace-web',
+        '@opentelemetry/instrumentation-xml-http-request'
+      ]
     }
   }
-});
+})
