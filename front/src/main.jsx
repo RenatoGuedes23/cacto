393a5245d437cb6b5f5d4db13c91a1fe82aac739
Renato Guedes <renatoguedesead@gmail.com>
2024-10-21 01:25:00 -0300

    adicionando debug no main

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,11 +1,15 @@
-import './otel-config'
+import './otel-config'  // Atualize o caminho conforme necessário
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+console.log('Inicializando aplicação...')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
+
+console.log('Aplicação inicializada com sucesso.')
