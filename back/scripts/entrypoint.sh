82374464be0e7b723a1494f1e6f3f31f915b2a57
Renato Guedes <renatoguedesead@gmail.com>
2024-10-24 04:21:39 -0300

    ajustando envs

--- back/scripts/entrypoint.sh
+++ back/scripts/entrypoint.sh
@@ -1,15 +1,19 @@
 # uvicorn main:app --reload --host 0.0.0.0 --port 8000
 # opentelemetry-instrument --logs_exporter otlp --traces_exporter otlp --metrics_exporter otlp --service_name ${APP_NAME} --exporter_otlp_endpoint ${COLLECTOR} uvicorn main:app --host ${APP_HOST} --port ${APP_PORT}
-opentelemetry-instrument --logs_exporter otlp --traces_exporter otlp --metrics_exporter otlp --service_name aci_api_service --exporter_otlp_endpoint http://otel-collector-collector.default.svc.cluster.local:4317 uvicorn main:app --host localhost --port 8000
+
+opentelemetry-instrument uvicorn main:app
+
 echo "Starting Aci-API Service with Uvicorn and OpenTelemetry"
 
-# echo "$COLLECTOR"
-# echo "$APP_NAME"
-# echo "$APP_HOST"
-# echo "$APP_PORT"
 
-# echo "$DATABASE_USER"
-# echo "$DATABASE_PASS"
-# echo "$DATABASE_HOST"
-# echo "$DATABASE_PORT"
+echo "$APP_NAME"
+echo "$APP_HOST"
+echo "$APP_PORT"
+echo "$OTEL_SERVICE_NAME"
+echo "$OTEL_EXPORTER_OTLP_ENDPOINT" 
+echo "$OTEL_EXPORTER_OTLP_INSECURE"
+echo "$OTEL_LOGS_EXPORTER"
+echo "$OTEL_METRICS_EXPORTER"
+echo "$OTEL_TRACES_EXPORTER"
+
 
